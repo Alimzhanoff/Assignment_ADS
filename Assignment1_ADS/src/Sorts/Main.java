51e79ca1133284b096646a198d21ffef225025f7
Alimzhanoff <garnetik08@gmail.com>
2026-04-28 12:21:37 +0500

    The Assignment #2 code is added!

--- Assignment1_ADS/src/Sorts/Main.java
+++ Assignment1_ADS/src/Sorts/Main.java
@@ -4,10 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ADS Assignment 3: Sorting and Searching Analysis");
 
-        // Создаем объект эксперимента [cite: 54]
+
         Experiment experiment = new Experiment();
 
-        // Запускаем все тесты [cite: 39, 64]
         experiment.runAllExperiments();
 
         System.out.println("All experiments finished.");
